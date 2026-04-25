6d79a84987135a61612005c8c38691b3fea93ec4
nanak06 <nanak.barring@campus.mccd.edu>
2026-04-25 04:47:37 +0000

    Added comments to Action class

--- src/Action.java
+++ src/Action.java
@@ -1,9 +1,15 @@
+// Action class represents something an NPC can do
 public class Action {
+
+    // Name of the action, such as Eat, Sleep, or Explore
     private String name;
+
+    // These values change the NPC's stats when the action happens
     private int hungerChange;
     private int energyChange;
     private int moodChange;
 
+    // Constructor initializes the action name and stat effects
     public Action(String name, int hungerChange, int energyChange, int moodChange) {
         this.name = name;
         this.hungerChange = hungerChange;
@@ -11,6 +17,7 @@ public class Action {
         this.moodChange = moodChange;
     }
 
+    // Getter methods
     public String getName() {
         return name;
     }
@@ -27,6 +34,7 @@ public class Action {
         return moodChange;
     }
 
+    // Applies this action's effects to an NPC
     public void applyTo(NPC npc) {
         npc.changeStats(hungerChange, energyChange, moodChange);
     }
